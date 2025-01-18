6aa42fa01ca54e3deb74772bf894cdbdc326aee7
Nura Programmer <nuraprogrammer@gmail.com>
2025-01-18 02:04:25 -0500

    Task 7: Implement loggin in as a registered user

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -10,13 +10,32 @@ const isValid = (username)=>{ //returns boolean
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    return users.find((u) => u.username === username && u.password === password) ? true : false;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+
+    // Validate if username and password are provided
+    if (!username || !password) {
+        return res.status(400).json({ message: "Username and password are required" });
+    }
+
+    // Check if the user exists and the password matches
+    if (!authenticatedUser(username, password)) {
+        return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    // Generate JWT token
+    const accessToken = jwt.sign({ username }, "access", { expiresIn: "1h" });
+
+    // Save the accessToken and username in the session
+    req.session.authorization  = {
+        accessToken, username
+    }
+
+    res.status(200).json({ message: "Login successful" });
 });
 
 // Add a book review
