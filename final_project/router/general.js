835f932a1f702792a5f2ab050ab8493d550dd9bf
Nura Programmer <nuraprogrammer@gmail.com>
2025-01-18 01:43:59 -0500

    Task-1: Implement getting list of books available in the shop

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,8 +12,9 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+   // Get the list of all books
+    const booksList = JSON.stringify(books, null, 4); // Neatly format the JSON output with 4 spaces indentation
+    return res.status(200).send(booksList); // Send the books as the response
 });
 
 // Get book details based on ISBN
