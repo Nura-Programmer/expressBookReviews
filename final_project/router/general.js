29fe183d0c2da285bc245721cb9ea2ef4db65c57
Nura Programmer <nuraprogrammer@gmail.com>
2025-01-18 01:54:13 -0500

    Task 5: Implement getting book reviews

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -68,8 +68,17 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Retrieve the ISBN from the request parameters
+  const isbn = req.params.isbn;
+
+  // Check if the book with the given ISBN exists
+  const book = books[isbn];
+
+  if (book) {
+      return res.status(200).json(book.reviews); // Send the reviews of the book as the response
+  } else {
+      return res.status(404).json({ message: "Book not found" }); // If the book does not exist
+  }
 });
 
 module.exports.general = public_users;
